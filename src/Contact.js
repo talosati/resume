56e7fbcede9840a62b621560bd87282bc562e48b
talosati <talos.barbara@gmail.com>
2020-09-11 17:22:40 +0200

    contact form is ready

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form} from 'react-bootstrap'
 import emailjs from 'emailjs-com';
+
 // import ReCAPTCHA from "react-google-recaptcha";
 //
 // dotenv.config();
@@ -14,7 +17,7 @@ function Contact() {
 
     function sendEmail(e) {
         e.preventDefault();
-        emailjs.sendForm('gmail', 'template_1jb72nh', e.target, 'user_arXYp4I4EW74lEp92cYT1')
+        emailjs.sendForm('gmail', 'template_lq7wgyd', e.target, 'user_arXYp4I4EW74lEp92cYT1')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -24,22 +27,32 @@ function Contact() {
     }
 
     return (
-        <form className="contact-form" onSubmit={sendEmail}>
-            {/*<div>*/}
-            {/*<ReCAPTCHA sitekey={process.env.PUBLIC_RECAPTCHA_SITE_KEY} onChange={onChange()} />*/}
-            {/*</div>*/}
-            <div>
-                <label>Name</label>
-                <input type="text" name="user_name"/>
-                <label>Email</label>
-                <input type="email" name="user_email"/>
-                <label>Subject</label>
-                <input type="subject" name="subject"/>
-                <label>Message</label>
-                <textarea name="message"/>
-                <input type="submit" value="Send"/>
-            </div>
-        </form>
+        <div className='w-50 mx-auto p-3 mt-2'>
+            <form className="contact-form" onSubmit={sendEmail}>
+                {/*<div>*/}
+                {/*<ReCAPTCHA sitekey={process.env.PUBLIC_RECAPTCHA_SITE_KEY} onChange={onChange()} />*/}
+                {/*</div>*/}
+                <Form>
+                    <Form.Group controlId="formGroupUsername">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="user_name"/>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name='user_email'/>
+                    </Form.Group>
+                    <Form.Group controlId="subject">
+                        <Form.Label>Subject</Form.Label>
+                        <Form.Control type="subject" name='subject'/>
+                    </Form.Group>
+                    <Form.Group controlId="textarea">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows="10" name='message'/>
+                    </Form.Group>
+                    <Button variant="dark" type='submit' value='Send'>Send</Button>
+                </Form>
+            </form>
+        </div>
     );
 }
 
