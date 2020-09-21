111755e10c381d865e6e3cd140695e82b01014ce
talosati <talos.barbara@gmail.com>
2020-09-21 14:19:00 +0200

    about typo and link css corrected

--- src/About.js
+++ src/About.js
@@ -13,20 +13,20 @@ function About() {
             <div className="about">
                 <Table striped bordered hover variant="dark">
                     <tbody>
-                        <tr>
-                            <td>email</td>
-                            <td>mobile</td>
-                            <td>GitHub</td>
-                            <td>HackerRank</td>
-                            <td>CodeWars</td>
-                        </tr>
-                        <tr>
-                            <td><a href="/contact">talos.atanaz@gmail.com</a></td>
-                            <td>+36309738761</td>
-                            <td><a href="https://github.com/talosati" target="_blank">talosati</a></td>
-                            <td><a href="https://www.hackerrank.com/atanaz" target="_blank">atanaz</a></td>
-                            <td><a href="https://www.codewars.com/users/talosatanaz" target="_blank">talosatanaz</a></td>
-                        </tr>
+                    <tr>
+                        <td>email</td>
+                        <td>mobile</td>
+                        <td>GitHub</td>
+                        <td>HackerRank</td>
+                        <td>CodeWars</td>
+                    </tr>
+                    <tr>
+                        <td><a href="/contact">talos.atanaz@gmail.com</a></td>
+                        <td>+36309738761</td>
+                        <td><a href="https://github.com/talosati" target="_blank">talosati</a></td>
+                        <td><a href="https://www.hackerrank.com/atanaz" target="_blank">atanaz</a></td>
+                        <td><a href="https://www.codewars.com/users/talosatanaz" target="_blank">talosatanaz</a></td>
+                    </tr>
                     </tbody>
                 </Table>
                 <div className="onlyPrintAndMobile">
@@ -34,13 +34,15 @@ function About() {
                     <Container>
                         <Row>
                             <Col md={6}>
-                                <p>My resume website: <a href="https://atanastalos.com/" target="_blank">atanastalos.com</a></p>
+                                <p>My resume website: <a href="https://atanastalos.com/"
+                                                         target="_blank">atanastalos.com</a></p>
                                 <p>Email: <a href="mailto:talos.atanaz@gmail.com">talos.atanaz@gmail.com</a></p>
                                 <p>Mobile: +36309738761</p>
                             </Col>
                             <Col md={6}>
                                 <p>Github: <a href="https://github.com/talosati" target="_blank">talosati</a></p>
-                                <p>HackerRank: <a href="https://www.hackerrank.com/atanaz" target="_blank">atanaz</a></p>
+                                <p>HackerRank: <a href="https://www.hackerrank.com/atanaz" target="_blank">atanaz</a>
+                                </p>
                                 <p>CodeWars: <a href="https://www.codewars.com/users/talosatanaz"
                                                 target="_blank">talosatanaz</a></p>
                             </Col>
@@ -48,34 +50,41 @@ function About() {
                     </Container>
                 </div>
                 <p>
-                    I graduated from the <a href="https://www.greenfoxacademy.com/en/home">Green Fox Academy</a> bootcamp this year as a full-stack developer.
+                    I graduated from the <a href="https://www.greenfoxacademy.com/en/home" target="_blank">Green Fox
+                    Academy</a> bootcamp this year as a full-stack developer.
                     The
                     technologies covered were Java (with Spring framework) and JavaScript (Express, React).
                     During
                     this 4 months long bootcamp I have proven capable of learning new technologies quickly and
-                    independently. During the project phase of the course we worked on a project together, and learned how to
-                    work a team led by a senior mentor. We followed Agile practices, amd all of our agile meetings were facilitated in English.
+                    independently. During the project phase of the course we worked on a project together, and learned
+                    how to
+                    work a team led by a senior mentor. We followed Agile practices, and all of our agile meetings were
+                    facilitated in English.
                 </p>
                 <p>
                     Before the course I used HTML and CSS to design my blogs as a hobby, and I also
                     graduated as
                     a graphic designer to extend my knowledge about creating unique and nice web pages.
-                    Before turning towards software engineering, I primarily worked as a certified accountant; I have an accounting and finance degree. In this phase of my career I worked primarily with businesses, and grew from assistan bookeeper into a team lead, where I also managed the cooperation between colleagues and organized subtasks.
+                    Before turning towards software engineering, I primarily worked as a certified accountant; I have an
+                    accounting and finance degree. In this phase of my career I worked primarily with businesses, and
+                    grew from assistant bookeeper into a team lead, where I also managed the cooperation between
+                    colleagues and organized subtasks.
                 </p>
                 <p>
                     As bookkeeper I enjoyed solving problems with precision, combining theoretical and
                     practical
                     knowledge, using a systematic approach. I appreciated the life-long learning
-                    and constant new challenges, but I wanted a profession where I can use the acquired soft skills in a more creative
+                    and constant new challenges, but I wanted a profession where I can use the acquired soft skills in a
+                    more creative
                     environment, so I decided to take my career in a new direction and become a software engineer.
                 </p>
-                <p> I am a trangender activist and I used my organizational skills as a co-organizer of the first transgender pride demonstration in Hungary.
-
-                    I am based in Berlin. </p>
+                <p> I am a trangender activist and I used my organizational skills as a co-organizer of the first
+                    transgender pride demonstration in Hungary.</p>
+                <p>I am based in Berlin. </p>
             </div>
         </>
     )
-    ;
+        ;
 }
 
 export default About;
