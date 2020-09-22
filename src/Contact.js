104d13d6d4eeb04ca48c5caa664730d94089854a
talosati <talos.barbara@gmail.com>
2020-09-22 14:50:37 +0200

    Contact.js extended with hooks

--- src/Contact.js
+++ src/Contact.js
@@ -1,45 +1,66 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form} from 'react-bootstrap'
+import {Alert, Button, Form} from 'react-bootstrap'
 import emailjs from 'emailjs-com';
 
 function Contact() {
+
+    const [trigger, setTrigger] = useState(false);
+    const prevTrigger = useRef('');
+    const [resultOfSendingEmail, setResultOfSendingEmail] = useState('');
+    const [design, setDesign] = useState('');
 
     function sendEmail(e) {
         e.preventDefault();
         emailjs.sendForm('gmail', 'template_lq7wgyd', e.target, 'user_arXYp4I4EW74lEp92cYT1')
             .then((result) => {
-                alert("Thank you for your email!");
+                setResultOfSendingEmail('Thank you for your email!');
+                setDesign('success');
             }, (error) => {
-                alert(error.text);
+                setResultOfSendingEmail(error.text);
+                setDesign('danger');
             });
         e.target.reset();
     }
 
+    useEffect(() => {
+        prevTrigger.current = trigger;
+    }, [trigger]);
+
+    function tryToSendEmail() {
+        return <Alert variant={design}>{resultOfSendingEmail}</Alert>
+    }
+
     return (
-        <div className='w-50 mx-auto p-3 mt-2'>
-            <form className="contact-form" onSubmit={sendEmail}>
-                <Form>
-                    <Form.Group controlId="formGroupUsername">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" name="name"/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" name='email'/>
-                    </Form.Group>
-                    <Form.Group controlId="subject">
-                        <Form.Label>Subject</Form.Label>
-                        <Form.Control type="subject" name='subject'/>
-                    </Form.Group>
-                    <Form.Group controlId="textarea">
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control as="textarea" rows="10" name='message'/>
-                    </Form.Group>
-                    <Button variant="dark" type='submit' value='Send'>Send</Button>
-                </Form>
-            </form>
-        </div>
+        <>
+            <div className="alert">
+                {(prevTrigger !== trigger) ? tryToSendEmail() : null}
+            </div>
+            <div className='w-50 mx-auto p-3 mt-2'>
+                <form className="contact-form" onSubmit={sendEmail}>
+                    <Form>
+                        <Form.Group controlId="formGroupUsername">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" name="name"/>
+                        </Form.Group>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name='email'/>
+                        </Form.Group>
+                        <Form.Group controlId="subject">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control type="subject" name='subject'/>
+                        </Form.Group>
+                        <Form.Group controlId="textarea">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control as="textarea" rows="10" name='message'/>
+                        </Form.Group>
+                        <Button onClick={() => setTrigger(!trigger)} variant="dark" type='submit'
+                                value='Send'>Send</Button>
+                    </Form>
+                </form>
+            </div>
+        </>
     );
 }
 
