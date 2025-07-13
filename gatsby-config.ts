1b00f1d812a9f2b580d901d46e902b7a95999a5a
atanaz-admin-at-783396382969 <talos.atanaz@gmail.com>
2025-07-13 19:57:29 +0200

    create new Gatsby project communicating with Contentful

---
+++ gatsby-config.ts
@@ -0,0 +1,24 @@
+import type { GatsbyConfig } from "gatsby";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Resume`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": process.env.ACCESS_TOKEN,
+      "spaceId": process.env.SPACE_ID
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+};
+
+export default config;
