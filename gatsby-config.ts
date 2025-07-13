1e8ae94dee6f79ac28bb929328a58e9daf7f384f
atanaz-admin-at-783396382969 <talos.atanaz@gmail.com>
2025-07-13 20:09:04 +0200

    add public info

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,12 +6,13 @@ dotenv.config();
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Resume`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.atanastalos.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: "/resume",
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
